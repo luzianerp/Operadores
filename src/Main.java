39ebacd88549b29516eaabce9fde98528dd7a8cd
luzianerp <rpluziane@gmail.com>
2024-09-27 10:40:13 -0300

    Operadores

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Operadores - Soma");
+        int valor =  5;  //Atribui o valor 5 à variável valor
+
+        System.out.println(Valor);
+
+    }
+}
